d307ece4a02646ff7ee81020677bff0a22fcf454
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-09-11 17:18:39 +0100

    Gather form data

--- ajax_form/index.php
+++ ajax_form/index.php
@@ -42,16 +42,32 @@
     result_div.style.display = 'none';
   }
 
+  // omits textareas, select-options, checkboxes, radio buttons
+  function gatherFormData(form) {
+    var inputs = form.getElementsByTagName('input');
+    var array = [];
+    for(i=0; i < inputs.length; i++) {
+      var inputNameValue = inputs[i].name + '=' + inputs[i].value;
+      array.push(inputNameValue);
+    }
+    return array.join('&');
+  }
+
   function calculateMeasurements() {
     clearResult()
 
     var form = document.getElementById("measurement-form");
     var action = form.getAttribute("action");
 
+    var form_data = new FormData(form);
+    for ([key, value] of form_data.entries()) {
+      console.log(key + ': ' + value);
+    }
 
     var xhr = new XMLHttpRequest();
     xhr.open('POST', action, true);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    // Do not set Content-type with FormData
+    // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.onreadystatechange = function () {
       if(xhr.readyState == 4 && xhr.status == 200) {
@@ -60,7 +76,7 @@
         postResult(result);
       }
     };
-    xhr.send(form-data);
+    xhr.send(form_data);
   }
 
   var button = document.getElementById("ajax-submit");
