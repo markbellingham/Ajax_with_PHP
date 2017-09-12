8649b5965cd9366ecb2c3edf459f6487fcf6e2e3
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-09-12 22:49:53 +0100

    Allow fallback to HTML

--- ajax_form/process_measurements.php
+++ ajax_form/process_measurements.php
@@ -16,18 +16,26 @@ if($width == '') { $errors[] = 'width'; }
 if($height == '') { $errors[] = 'height'; }
 
 if(!empty($errors)) {
-  // Won't work because of single quotes
-  // echo "{ 'errors': " . json_encode($errors) . "}";
-  $result_array = array('errors' => $errors);
-  echo json_encode($result_array);
+  if(is_ajax_request()) {
+    // Won't work because of single quotes
+    // echo "{ 'errors': " . json_encode($errors) . "}";
+    $result_array = array('errors' => $errors);
+    echo json_encode($result_array);
+    exit;
+  } else {
+    echo "<p>There were errors on: " . implode(', ', $errors) . "</p>";
+    echo "<p><a href=\"index.php\">Back</a></p>";
+  }
   exit;
 }
+
 $volume = $length * $width * $height;
 
 if(is_ajax_request()) {
   echo json_encode(array('volume' => $volume));
 } else {
-  exit;
+    echo "<p>The total volume is: " . $volume . "</p>";
+    echo "<p><a href=\"index.php\">Back</a></p>";
 }
 
 ?>
