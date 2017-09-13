83adb2ed220264971486a8513ea05b88e6018e4e
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-09-13 17:25:40 +0100

    Prevent multiple requests

--- ajax_scroll/index.php
+++ ajax_scroll/index.php
@@ -15,24 +15,6 @@
 </head>
 <body>
   <div id="blog-posts">
-    <!-- <div id="blog-post-101" class="blog-post">
-      <h3>Blog Post 101</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt augue sed diam sagittis maximus. Nunc justo eros, tempor a bibendum nec, efficitur aliquet enim. Sed quis eros neque. Aenean est arcu, tincidunt blandit blandit eget, semper id arcu. Fusce non sapien eget elit feugiat commodo. Curabitur vel odio nunc. Proin sit amet bibendum ex, fermentum posuere leo. Etiam facilisis venenatis nunc, nec vestibulum lacus faucibus in. In hac habitasse platea dictumst. Maecenas eu bibendum mi, non porttitor lacus. Curabitur at auctor ante. Sed efficitur arcu ornare tempor dictum. Nulla commodo risus lectus, at commodo arcu dapibus sit amet. Nunc finibus commodo sem vel bibendum. Ut eget facilisis lacus.
-      </p>
-    </div> <!-- ends blog-post-101 -->
-    <!-- <div id="blog-post-102" class="blog-post">
-      <h3>Blog Post 102</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget bibendum mauris. Sed viverra vestibulum faucibus. Cras pharetra lacinia libero, facilisis tincidunt mi consequat a. Cras efficitur sollicitudin velit sed vehicula. Nam ac mi tincidunt, venenatis nisl eu, venenatis risus. Proin diam ex, semper eu quam sed, tempor egestas nisi. Maecenas tincidunt gravida lacus, porta ullamcorper velit efficitur aliquet. Aenean volutpat nunc et diam ullamcorper efficitur. Phasellus erat nulla, egestas non erat sed, convallis venenatis sem. Integer tempus faucibus lectus, et finibus sapien malesuada quis. Vestibulum at dui dignissim justo rutrum condimentum quis pulvinar arcu. Phasellus et vehicula mi. Donec ut egestas lectus, a malesuada ante.
-      </p>
-    </div> <!-- ends blog-post-102 --> -->
-    <!-- <div id="blog-post-103" class="blog-post">
-      <h3>Blog Post 103</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id turpis euismod sem malesuada molestie. Nunc finibus pulvinar risus, vel dapibus lorem ullamcorper non. Duis volutpat purus non volutpat tempor. Aenean dignissim eleifend risus, at mattis augue elementum consequat. Donec vitae augue convallis, venenatis enim sed, malesuada nulla. Fusce et leo velit. Sed a ligula vel libero pharetra sagittis quis vitae nunc. Praesent sit amet magna pellentesque, facilisis augue nec, fringilla turpis.
-      </p>
-    </div> <!-- ends blog-post-103 --> --> -->
   </div> <!-- ends blog-posts -->
 
   <div id="spinner">
@@ -47,6 +29,7 @@
 
   var container = document.getElementById('blog-posts');
   var load_more = document.getElementById('load-more');
+  var request_in_progress = false;
 
   function showSpinner() {
     var spinner = document.getElementById("spinner");
@@ -91,13 +74,16 @@
   function scrollReaction() {
     var content_height = container.offsetHeight;
     var current_y = window.innerHeight + window.pageYOffset;
-    console.log(current_y + '/' + content_height);
+    // console.log(current_y + '/' + content_height);
     if(current_y >= content_height) {
       loadMore();
     }
   }
 
   function loadMore() {
+
+    if(request_in_progress) { return; }
+    request_in_progress = true;
 
     showSpinner()
     hideLoadMore()
@@ -118,7 +104,7 @@
         // append results to end of blog posts
         appendToDiv(container, result);
         showLoadMore();
-
+        request_in_progress = false;
       }
     };
     xhr.send();
@@ -130,7 +116,7 @@
   }
 
   // Load even the first page with Ajax
-  // loadMore();
+  loadMore();
 
   </script>
 </body>
